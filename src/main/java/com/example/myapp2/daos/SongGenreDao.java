ee6a4de95a770ad60e66c50e7ac2b255da8bea59
Maggie Navracruz <mnavracruz@Maggies-MacBook-Pro-2.local>
2020-12-03 21:32:21 -0500

    Updated song genres

--- src/main/java/com/example/myapp2/daos/SongGenreDao.java
+++ src/main/java/com/example/myapp2/daos/SongGenreDao.java
@@ -1,10 +1,13 @@
 package com.example.myapp2.daos;
 
 import com.example.myapp2.models.Genre;
+import com.example.myapp2.models.Song;
 import com.example.myapp2.models.SongGenre;
 import com.example.myapp2.repositories.GenreRepository;
 import com.example.myapp2.repositories.SongGenreRepository;
 import com.example.myapp2.repositories.SongRepository;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +29,7 @@ public class SongGenreDao {
   }
 
   @GetMapping("/findGenreBySong/{songId}")
-  public Optional<Genre> findGenreBySongId (
+  public Optional<Genre> findGenreBySong (
       @PathVariable("songId") Integer songId) {
     Optional<Genre> genre = null;
     Iterable<SongGenre> songGenres = songGenreRepository.findAll();
@@ -37,5 +40,19 @@ public class SongGenreDao {
       }
     }
     return genre;
+  }
+
+  @GetMapping("/findSongsByGenre/{genreId}")
+  public List<Optional<Song>> findSongsByGenre (
+      @PathVariable("genreId") Integer genreId) {
+    List<Optional<Song>> songs = new ArrayList<>();
+    Iterable<SongGenre> songGenres = songGenreRepository.findAll();
+    for (SongGenre sg : songGenres) {
+      if (sg.getGenreId() == genreId) {
+        Integer currentSong = sg.getSongId();
+        songs.add(songRepository.findById(currentSong));
+      }
+    }
+    return songs;
   }
 }
