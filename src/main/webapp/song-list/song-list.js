2b6e2147d75b5977bc14b752911fe364044208d6
Maggie Navracruz <mnavracruz@gmail.com>
2020-12-08 12:09:37 -0500

    Added artist loading state

--- src/main/webapp/song-list/song-list.js
+++ src/main/webapp/song-list/song-list.js
@@ -1,19 +1,23 @@
 class SongList extends React.Component {
   state = {
-    songs: []
+    songs: [],
+    artistLoading: true
   }
 
   findSongArtists = (songId) =>
       findSongArtists(songId)
       .then(response => response)
 
-  findAllSongs = () =>
-      findAllSongs()
-      .then((songs) => {
-        songs.map((song) =>
-            this.findSongArtists(song.id).then((response) => song.artists = response))
-        this.setState({songs: songs})
-      })
+  findAllSongs = () => {
+    return findAllSongs()
+    .then((songs) => {
+      songs.map((song) =>
+          this.findSongArtists(song.id).then(
+              response => song.artists = response).finally(
+                  () => this.setState({artistLoading: false})))
+      this.setState({songs: songs})
+    })
+  }
 
   createSong = () =>
       createSong(artistId, songName)
@@ -28,43 +32,48 @@ class SongList extends React.Component {
   }
 
   render() {
-    return(
-        <div className="container-fluid">
-          <button
-              onClick={() => this.createSong()}>
-            Create
-          </button>
-          <a  href="../../index.html">
-            Home
-          </a>
-          <h1>Songs</h1>
-          <table>
-            <tbody>
-            {
-              this.state.songs.map((song) =>
-                  <tr key={song.id}>
-                    <td>{song.name}</td>
-                    {song.artists}.map((artist) =>
-                    <td>{song.artists[artist]}</td>
-                    )
-                    <td>{song.duration}</td>
-                    <td>
-                      {/*<a className="btn btn-primary float-right"*/}
-                      {/*   href={`/course-editor/course-editor.html?courseId=${course.courseId}`}>*/}
-                      {/*  Edit*/}
-                      {/*</a>*/}
-                      <button className="btn btn-danger float-right"
-                              onClick={() => this.deleteSong(song.id)}>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-              )
-            }
-            </tbody>
-          </table>
-        </div>
-    )
+    console.log(this.state)
+      return (
+            this.state.artistLoading ?
+                <div>Loading...</div> :
+                <div className="container-fluid">
+                  <button
+                      onClick={() => this.createSong()}>
+                    Create
+                  </button>
+                  <a href="../../index.html">
+                    Home
+                  </a>
+                  <h1>Songs</h1>
+                  <table>
+                    <tbody>
+                    {
+                      this.state.songs.map((song) =>
+                          <tr key={song.id}>
+                            <td>{song.name}</td>
+                            { song.artists ?
+                            <td>{song.artists.map((artist) =>
+                              <p>{artist.userId}</p>
+                            )}</td>
+                                : <td></td> }
+                            <td>{song.duration}</td>
+                            <td>
+                              {/*<a className="btn btn-primary float-right"*/}
+                              {/*   href={`/course-editor/course-editor.html?courseId=${course.courseId}`}>*/}
+                              {/*  Edit*/}
+                              {/*</a>*/}
+                              <button className="btn btn-danger float-right"
+                                      onClick={() => this.deleteSong(song.id)}>
+                                Delete
+                              </button>
+                            </td>
+                          </tr>
+                      )
+                    }
+                    </tbody>
+                  </table>
+                </div>
+      )
   }
 }
 
